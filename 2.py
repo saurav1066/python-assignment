a01be31aaef104a5597f7da2f0962af5bc5fe77d
saurav <sauravadhikari35@gmail.com>
2020-06-23 18:31:14 +0545

    First 2 questions

--- 2.py
+++ 2.py
@@ -1,13 +1,16 @@
-# Write a Python program to
-# get a string made of the first 2 and the last 2 chars
-# from a given a string.
-# If the string length is less than 2, return instead of the empty string.
-# Sample String : 'Python'
-# Expected Result : 'Pyon'
-# Sample String : 'Py'
-# Expected Result : 'PyPy'
-# Sample String : ' w'
-# Expected Result : Empty String
+"""
+Write a Python program to get a string made of the first 2 and the last 2 chars from a given a string.
+If the string length is less than 2, return instead of the empty string.
+Sample String : 'Python'
+Expected Result : 'Pyon'
+Sample String : 'Py'
+Expected Result : 'PyPy'
+Sample String : ' w'
+Expected Result : Empty String
+"""
 
 inp = input("Enter the string:")
-
+if len(inp) < 2:
+    print('')
+else:
+    print(inp[0:2] + inp[-2:])
