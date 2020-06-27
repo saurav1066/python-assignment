3884a9bf15e9886883fa61be3724a84c9260503a
saurav <sauravadhikari35@gmail.com>
2020-06-27 19:13:37 +0545

    All completed except ques 14

--- b19.py
+++ b19.py
@@ -1,15 +1,8 @@
 """
 Write a Python program to create Fibonacci series upto n using Lambda.
 """
-from functools import reduce
 
 inp = int(input("Enter a number:"))
-
-fib_series = lambda n: reduce(lambda x, _: x + [x[-1] + x[-2]],
-                              range(n - 2), [0, 1])
-print(f'fibonacci sequence {fib_series(inp)}')
-
-"""
-I have no idea how this worked...
-
-"""
+fibo = lambda num: num if num <= 1 else fibo(num-1)+fibo(num-2)
+fibo_getter = list(map(fibo, range(0, inp)))
+print(fibo_getter)
