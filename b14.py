bd051ab33d68e8b85f66662a85281faf8b6e27f5
saurav <sauravadhikari35@gmail.com>
2020-06-28 16:12:03 +0545

    Assignment Completed

--- b14.py
+++ b14.py
@@ -2,3 +2,6 @@
 Write a Python program to sort a list of dictionaries using Lambda.
 """
 
+lis = [{"name": "Saurav", "age": 23}, {"name": "sujan", "age": 12}, {"name": "Hitesh", "age": 15}]
+value_fetch = lambda i: i['age']
+print(sorted(lis, key=value_fetch))
